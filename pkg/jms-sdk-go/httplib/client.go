e6007f84016759f8039b8d96e9a43d5af4b9bff2
Eric <xplzv@126.com>
2022-01-17 14:24:55 +0800

    fix: 修复无权限的错误提示

--- pkg/jms-sdk-go/httplib/client.go
+++ pkg/jms-sdk-go/httplib/client.go
@@ -163,12 +163,9 @@ func (c *Client) Do(method, reqUrl string, data, res interface{}, params ...map[
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	if resp.StatusCode >= 400 {
-		msg := fmt.Sprintf("%s %s failed, get code: %d, %s", req.Method, req.URL, resp.StatusCode, body)
-		err = errors.New(msg)
-		return
+	if err != nil {
+		return resp, err
 	}
-
 	// If is buffer return the raw response body
 	if buf, ok := res.(*bytes.Buffer); ok {
 		buf.Write(body)
@@ -185,6 +182,12 @@ func (c *Client) Do(method, reqUrl string, data, res interface{}, params ...map[
 				return
 			}
 		}
+	}
+
+	if resp.StatusCode >= 400 {
+		msg := fmt.Sprintf("%s %s failed, get code: %d, %s", req.Method, req.URL, resp.StatusCode, body)
+		err = errors.New(msg)
+		return
 	}
 	return
 }
