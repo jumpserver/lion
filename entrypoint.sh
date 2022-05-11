3c7c15aa69bcc0798d0111d7f338164001343d0b
Eric <xplzv@126.com>
2022-05-11 23:31:47 +0800

    perf: guacd 日志级别可配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,23 @@
 # 需要先创建 guacd 存放日志的目录
 mkdir -p /opt/lion/data/logs/
 
+if [ $LOG_LEVEL ]; then
+        level="info"
+        case $LOG_LEVEL in
+        "DEBUG")
+                level="debug"
+                ;;
+        "INFO")
+                level='info'
+                ;;
+        "WARN")
+                level='warning'
+                ;;
+        "ERROR" | "FATAL" | "CRITICAL")
+                level='error'
+                ;;
+        esac
+        export GUACD_LOG_LEVEL=$level
+fi
+
 /usr/bin/supervisord
