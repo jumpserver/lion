a4b1dc081f10dae2c3ba5648eae63a0940bff441
Eric <xplzv@126.com>
2022-05-11 11:46:07 +0800

    perf: 修改右侧栏的展示

--- ui/src/utils/common.js
+++ ui/src/utils/common.js
@@ -83,3 +83,34 @@ export function getCookie(name) {
   const match = document.cookie.match(new RegExp(name + '=([^;]+)'))
   return match ? match[1] : undefined
 }
+
+/**
+ * Check if an element has a class
+ * @param {HTMLElement} elm
+ * @param {string} cls
+ * @returns {boolean}
+ */
+export function hasClass(ele, cls) {
+  return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'))
+}
+
+/**
+ * Add class to element
+ * @param {HTMLElement} elm
+ * @param {string} cls
+ */
+export function addClass(ele, cls) {
+  if (!hasClass(ele, cls)) ele.className += ' ' + cls
+}
+
+/**
+ * Remove class from element
+ * @param {HTMLElement} elm
+ * @param {string} cls
+ */
+export function removeClass(ele, cls) {
+  if (hasClass(ele, cls)) {
+    const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)')
+    ele.className = ele.className.replace(reg, ' ')
+  }
+}
