36549038bfba8737d481558ef2d418d6ed715035
Eric <xplzv@126.com>
2022-12-13 12:12:44 +0800

    perf: api secret_type field change

--- pkg/jms-sdk-go/model/account.go
+++ pkg/jms-sdk-go/model/account.go
@@ -3,14 +3,18 @@ package model
 import "fmt"
 
 type BaseAccount struct {
-	Name       string `json:"name"`
-	Username   string `json:"username"`
-	Secret     string `json:"secret"`
-	SecretType string `json:"secret_type"`
+	Name       string     `json:"name"`
+	Username   string     `json:"username"`
+	Secret     string     `json:"secret"`
+	SecretType LabelValue `json:"secret_type"`
 }
 
 func (a *BaseAccount) String() string {
 	return fmt.Sprintf("%s(%s)", a.Name, a.Username)
+}
+
+func (a *BaseAccount) IsSSHKey() bool {
+	return a.SecretType.Value == "ssh_key"
 }
 
 type Account struct {
