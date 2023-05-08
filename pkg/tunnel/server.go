8871191efe9988cd7abae8bcf7d5c62aa6e675bf
Eric <xplzv@126.com>
2023-05-08 16:00:50 +0800

    perf: 增加键盘布局配置

--- pkg/tunnel/server.go
+++ pkg/tunnel/server.go
@@ -67,7 +67,9 @@ func (g *GuacamoleTunnelServer) getClientInfo(ctx *gin.Context) guacd.ClientInfo
 			info.OptimalResolution = dpiInt
 		}
 	}
-
+	if keyboardLayout, ok := ctx.GetQuery("GUAC_KEYBOARD"); ok {
+		info.KeyboardLayout = keyboardLayout
+	}
 	return info
 }
 
@@ -124,6 +126,9 @@ func (g *GuacamoleTunnelServer) Connect(ctx *gin.Context) {
 	}
 	info := g.getClientInfo(ctx)
 	conf := tunnelSession.GuaConfiguration()
+	for argName, argValue := range info.ExtraConfig() {
+		conf.SetParameter(argName, argValue)
+	}
 	// 设置网域网关，替换本地。 兼容云平台同步 配置网域，但网关配置为空的情况
 	if (tunnelSession.Domain != nil && len(tunnelSession.Domain.Gateways) != 0) ||
 		tunnelSession.Gateway != nil {
