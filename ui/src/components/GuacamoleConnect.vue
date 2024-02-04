913c43ca84c834c917e11c92945b6e2830d401cb
Eric <xplzv@126.com>
2024-02-04 11:15:41 +0800

    perf: 添加键盘鼠标续期

--- ui/src/components/GuacamoleConnect.vue
+++ ui/src/components/GuacamoleConnect.vue
@@ -550,7 +550,7 @@ export default {
       // Do not attempt to handle mouse state changes if the client
       // or display are not yet available
       if (!this.client || !this.display) { return }
-
+      this.sendEventToLuna('MOUSEEVENT', '')
       // Send mouse state, show cursor if necessary
       this.display.showCursor(!this.localCursor)
       this.sendScaledMouseState(mouseState)
@@ -787,9 +787,11 @@ export default {
       // Keyboard
       const keyboard = new Guacamole.Keyboard(sink.getElement())
       keyboard.onkeydown = (keysym) => {
+        this.sendEventToLuna('KEYBOARDEVENT', '')
         this.client.sendKeyEvent(1, keysym)
       }
       keyboard.onkeyup = (keysym) => {
+        this.sendEventToLuna('KEYBOARDEVENT', '')
         this.client.sendKeyEvent(0, keysym)
       }
       this.keyboard = keyboard
