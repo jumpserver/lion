f63edb78a5e2b61e35173428b3fbe6c266ea3931
Eric <xplzv@126.com>
2024-06-20 15:30:57 +0800

    perf: 超时连接修改成 5 分钟

--- ui/src/components/GuacamoleConnect.vue
+++ ui/src/components/GuacamoleConnect.vue
@@ -814,7 +814,7 @@ export default {
       const displayRef = document.getElementById('display')
       const tunnel = new Guacamole.WebSocketTunnel(wsURL)
       // 连接资产耗时，造成的 ws 超时断开问题 默认 15s 改成 60s
-      tunnel.receiveTimeout = 60 * 1000
+      tunnel.receiveTimeout = 5 * 60 * 1000
       const client = new Guacamole.Client(tunnel)
       this.client = client
       this.tunnel = tunnel
