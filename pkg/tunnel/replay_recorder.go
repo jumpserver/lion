b363cbc6f39724b590cc97fb1340503a23e2b5a0
Eric <xplzv@126.com>
2024-08-12 19:19:19 +0800

    perf: split session replay

---
+++ pkg/tunnel/replay_recorder.go
@@ -0,0 +1,160 @@
+package tunnel
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+
+	"lion/pkg/config"
+	"lion/pkg/guacd"
+	"lion/pkg/logger"
+	"lion/pkg/session"
+)
+
+const recordDirTimeFormat = "2006-01-02"
+
+func NewReplayRecorder(tunnelSession *session.TunnelSession, guacdAddr string, conf guacd.Configuration, info guacd.ClientInformation) *ReplayRecorder {
+	// 2006-01-02
+	conf.SetParameter(guacd.READONLY, "true")
+	recordDirPath := filepath.Join(config.GlobalConfig.RecordPath,
+		tunnelSession.Created.Format(recordDirTimeFormat))
+	sessionReplayRootPath := filepath.Join(recordDirPath, tunnelSession.ID)
+	_ = os.MkdirAll(sessionReplayRootPath, os.ModePerm)
+	return &ReplayRecorder{
+		tunnelSession: tunnelSession,
+		SessionId:     tunnelSession.ID,
+		conf:          conf,
+		RootPath:      sessionReplayRootPath,
+		guacdAddr:     guacdAddr,
+		info:          info,
+	}
+}
+
+type ReplayRecorder struct {
+	tunnelSession *session.TunnelSession
+	RootPath      string
+	SessionId     string
+	guacdAddr     string
+	conf          guacd.Configuration
+	info          guacd.ClientInformation
+	wg            sync.WaitGroup
+	currentIndex  int
+	newChan       chan struct{}
+}
+
+func (r *ReplayRecorder) run(ctx context.Context) {
+	r.startRecordPartReplay(ctx)
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Infof("replay conn %s done", r.SessionId)
+		case <-r.newChan:
+			r.currentIndex++
+			r.startRecordPartReplay(ctx)
+		}
+	}
+
+}
+
+func (r *ReplayRecorder) startRecordPartReplay(ctx context.Context) {
+	r.wg.Add(1)
+	go r.recordReplay(ctx, &r.wg)
+}
+
+func (r *ReplayRecorder) Start(ctx context.Context) {
+	if r.tunnelSession.TerminalConfig.ReplayStorage.TypeName == "null" {
+		logger.Infof("replay storage is null, not record")
+		return
+	}
+	go r.run(ctx)
+}
+
+func (r *ReplayRecorder) Stop() {
+	r.wg.Wait()
+	//
+}
+
+func (r *ReplayRecorder) recordReplay(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	path := filepath.Join(r.RootPath, strconv.Itoa(r.currentIndex))
+	fd, err := os.Create(path)
+	if err != nil {
+		logger.Errorf("create replay file %s failed: %v", path, err)
+		return
+	}
+	defer fd.Close()
+	writer := bufio.NewWriter(fd)
+	defer writer.Flush()
+	joinTunnel, err1 := guacd.NewTunnel(r.guacdAddr, r.conf, r.info)
+	if err1 != nil {
+		logger.Errorf("Join replay tunnel %s failed: %v", r.SessionId, err1)
+		return
+	}
+	defer joinTunnel.Close()
+	totalWriteSize := 0
+	disconnectInst := guacd.NewInstruction(guacd.InstructionClientDisconnect)
+	waitExit := false
+	for {
+		inst, err2 := joinTunnel.ReadInstruction()
+		if err2 != nil {
+			if waitExit && (err1 == io.EOF) {
+				logger.Infof("Join replay tunnel %s EOF", r.SessionId)
+				return
+			}
+			logger.Warnf("Join tunnel %s read failed: %v", r.SessionId, err2)
+			return
+		}
+		if inst.Opcode == INTERNALDATAOPCODE && len(inst.Args) >= 2 && inst.Args[0] == PINGOPCODE {
+			if err3 := joinTunnel.WriteInstruction(guacd.NewInstruction(INTERNALDATAOPCODE, PINGOPCODE)); err3 != nil {
+				fmt.Println("write internal data opcode ping failed: ", err3)
+			}
+			continue
+		}
+		select {
+		case <-ctx.Done():
+			if !waitExit {
+				_ = joinTunnel.WriteInstructionAndFlush(disconnectInst)
+				waitExit = true
+				logger.Infof("recordReplay ctx %s done", r.SessionId)
+			} else {
+				logger.Infof("recordReplay ctx %s done, wait exit", r.SessionId)
+			}
+		default:
+
+		}
+		switch inst.Opcode {
+		case guacd.InstructionClientSync:
+			_ = joinTunnel.WriteInstructionAndFlush(inst)
+		case guacd.InstructionClientNop:
+			logger.Debugf("join replay nop")
+			continue
+		default:
+		}
+		wr, err3 := writer.WriteString(inst.String())
+		if err3 != nil {
+			logger.Errorf("write replay file %s failed: %v", path, err3)
+		}
+		totalWriteSize += wr
+		if totalWriteSize > maxSize && !waitExit {
+			logger.Infof("write size > 50M, create new replay file")
+			_ = joinTunnel.WriteInstructionAndFlush(disconnectInst)
+			waitExit = true
+			r.newChan <- struct{}{}
+		}
+	}
+
+}
+
+const maxSize = 1024 * 1024 * 50
+
+func NewReplayConfiguration(conf *guacd.Configuration, connectionId string) guacd.Configuration {
+	newCfg := conf.Clone()
+	newCfg.ConnectionID = connectionId
+	newCfg.SetParameter(guacd.READONLY, "true")
+	return newCfg
+}
