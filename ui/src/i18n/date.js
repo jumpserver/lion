217a8baddf44f5eb4ff0ead1a881e23685c3d96a
Eric <xplzv@126.com>
2024-08-13 17:44:28 +0800

    perf: update i18n

--- ui/src/i18n/date.js
+++ ui/src/i18n/date.js
@@ -13,7 +13,21 @@ export default {
       hour: 'numeric', minute: 'numeric'
     }
   },
-  'cn': {
+  'zh': {
+    short: {
+      year: 'numeric', month: 'short', day: 'numeric'
+    },
+    medium: {
+      year: 'numeric', month: '2-digit', day: '2-digit',
+      hour: '2-digit', minute: '2-digit', second: '2-digit',
+      hourCycle: 'h23', hour12: false
+    },
+    long: {
+      year: 'numeric', month: 'short', day: 'numeric',
+      hour: 'numeric', minute: 'numeric', hour12: true
+    }
+  },
+  'zh_hant': {
     short: {
       year: 'numeric', month: 'short', day: 'numeric'
     },
