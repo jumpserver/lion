84e2d8b1083da36ff0702599a9b44bf6f72212b8
Eric <xplzv@126.com>
2025-07-16 14:50:24 +0800

    perf: modify keys style

--- ui/components.d.ts
+++ ui/components.d.ts
@@ -31,6 +31,7 @@ declare module 'vue' {
     NFormItemGi: typeof import('naive-ui')['NFormItemGi']
     NGi: typeof import('naive-ui')['NGi']
     NGrid: typeof import('naive-ui')['NGrid']
+    NIcon: typeof import('naive-ui')['NIcon']
     NInput: typeof import('naive-ui')['NInput']
     NList: typeof import('naive-ui')['NList']
     NListItem: typeof import('naive-ui')['NListItem']
