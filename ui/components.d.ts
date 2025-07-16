1f96ab930f7d5b7e99c317f296becf488f893c10
Eric <xplzv@126.com>
2025-07-16 17:11:03 +0800

    perf: change err msg style

--- ui/components.d.ts
+++ ui/components.d.ts
@@ -30,6 +30,7 @@ declare module 'vue' {
     NFormItem: typeof import('naive-ui')['NFormItem']
     NFormItemGi: typeof import('naive-ui')['NFormItemGi']
     NGi: typeof import('naive-ui')['NGi']
+    NGradientText: typeof import('naive-ui')['NGradientText']
     NGrid: typeof import('naive-ui')['NGrid']
     NIcon: typeof import('naive-ui')['NIcon']
     NInput: typeof import('naive-ui')['NInput']
