b8dda5de8756f0bc53c41eb5f6fe0fbbf1dbb029
fit2bot <68588906+fit2bot@users.noreply.github.com>
2025-11-18 17:08:16 +0800

    perf: useClipboard (#473)
    * perf: useClipboard
    
    * perf: Update Dockerfile with new base image tag
    
    ---------
    
    Co-authored-by: Eric <xplzv@126.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- ui/src/utils/clipboard.ts
+++ ui/src/utils/clipboard.ts
@@ -1,56 +1,21 @@
-export function copyToClipboard(text: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    if (navigator.clipboard && navigator.clipboard.writeText) {
-      navigator.clipboard
-        .writeText(text)
-        .then(() => resolve())
-        .catch((err) => reject(err));
-    } else {
-      // Fallback for browsers that do not support the Clipboard API
-      const textArea = document.createElement('textarea');
-      textArea.value = text;
-      textArea.style.position = 'fixed'; // Prevent scrolling to bottom of page in MS Edge.
-      document.body.appendChild(textArea);
-      textArea.focus();
-      textArea.select();
+import { useClipboardItems } from '@vueuse/core';
+const { content, read } = useClipboardItems();
 
-      try {
-        document.execCommand('copy');
-        resolve();
-      } catch (err) {
-        reject(err);
-      } finally {
-        document.body.removeChild(textArea);
-      }
-    }
+import { effect, shallowRef } from 'vue';
+const computedText = shallowRef('');
+const computedMimeType = shallowRef('');
+effect(() => {
+  Promise.all(content.value.map((item) => item.getType('text/plain'))).then(async (blobs) => {
+    computedMimeType.value = blobs.map((blob) => blob.type).join(', ');
+    computedText.value = (await Promise.all(blobs.map((blob) => blob.text()))).join(', ');
   });
-}
-
-export function pasteFromClipboard(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    if (navigator.clipboard && navigator.clipboard.readText) {
-      navigator.clipboard
-        .readText()
-        .then((text) => resolve(text))
-        .catch((err) => {
-          console.error('Failed to read from  Clipboard API :', err);
-          // reject(err);
-          resolve(''); // Return empty string on error
-        });
-    } else {
-      resolve(''); // Return empty string on error
-    }
-  });
-}
-
-export async function writeToClipboard(text: string): Promise<void> {
-  try {
-    await copyToClipboard(text);
-  } catch (err) {
-    console.error('Failed to write text to clipboard:', err);
-  }
-}
-
+});
 export async function readClipboardText(): Promise<string> {
-  return await pasteFromClipboard();
+  try {
+    await read();
+    return computedText.value;
+  } catch (err) {
+    console.error('Failed to read clipboard:', err);
+    return '';
+  }
 }
